8afafb4028fb6e7cd06025c58370ee2fd27506db
Pavel Grafkin <pavgra@gmail.com>
2019-06-25 12:36:51 -0400

    Substitute "default" as database ID in PLE analysis when the parameter is not passed

--- src/main/resources/resources/estimation/r/runAnalysis.R
+++ src/main/resources/resources/estimation/r/runAnalysis.R
@@ -26,7 +26,7 @@ tryCatch({
         resultsDatabaseSchema <- Sys.getenv("RESULT_SCHEMA")
         cohortsDatabaseSchema <- Sys.getenv("TARGET_SCHEMA")
         cohortTable <- Sys.getenv("COHORT_TARGET_TABLE")
-        databaseId <- Sys.getenv("DATA_SOURCE_NAME")
+        databaseId <- (function(dbId) if (is.na(dbId) || dbId == "") "default" else dbId)( Sys.getenv("DATA_SOURCE_NAME") )
         driversPath <- (function(path) if (path == "") NULL else path)( Sys.getenv("JDBC_DRIVER_PATH") )
 
         connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
