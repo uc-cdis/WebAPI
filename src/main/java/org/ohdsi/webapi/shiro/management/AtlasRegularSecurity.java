36d537dbb417a2d1f94e0205ae04b1ecda22e4ab
Andrew Prokhorenkov <aprokh@uchicago.edu>
2021-08-12 15:21:12 -0500

    fix: add only "user" scope

--- src/main/java/org/ohdsi/webapi/shiro/management/AtlasRegularSecurity.java
+++ src/main/java/org/ohdsi/webapi/shiro/management/AtlasRegularSecurity.java
@@ -321,7 +321,7 @@ public class AtlasRegularSecurity extends AtlasSecurity {
 
         if (this.openidAuthEnabled) {
             OidcConfiguration configuration = oidcConfCreator.build();
-            configuration.setScope("openid profile email user");
+            configuration.setScope("user");
             OidcClient oidcClient = new OidcClient(configuration);
             oidcClient.setCallbackUrl(oauthApiCallback);
             oidcClient.setCallbackUrlResolver(urlResolver);
